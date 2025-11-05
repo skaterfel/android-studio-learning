d800bcbdd7deb33d5613f450a5525c465523de10
Felipe L de Melo <felipe.leandro2@gmail.com>
2025-11-04 22:55:12 -0300

    iniciando estudos em Java / Android Studio

---
+++ PrimeiroApp/app/src/main/java/com/aprendendo/primeiroapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.aprendendo.primeiroapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void alterarTexto(View view){
+
+        TextView texto = findViewById(R.id.textoExibicao);
+        texto.setText("Clicou!");
+    }
+
+
+}
