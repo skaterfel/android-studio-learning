57444cb734453693cad99f25dcfaa0d2efd6d2bd
Felipe L de Melo <felipe.leandro2@gmail.com>
2025-11-12 13:52:52 -0300

    Feito app 'netflix' usando guideLines p/ alinhar objetos e assets vector do android studio

---
+++ JogodoDado/app/src/main/java/com/aprendendo/jogododado/MainActivity.java
@@ -0,0 +1,52 @@
+package com.aprendendo.jogododado;
+
+import static com.aprendendo.jogododado.R.id.dado;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ImageView dado;
+    private int[] numerosDados = {
+        R.drawable.dice1,
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6,
+    };
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+    public void rodaDado(View view) throws InterruptedException {
+        dado = findViewById(R.id.dado);
+        int numerosAleatorios = new Random().nextInt(numerosDados.length);
+        dado.setImageResource(numerosDados[numerosAleatorios]);
+
+
+    }
+}
