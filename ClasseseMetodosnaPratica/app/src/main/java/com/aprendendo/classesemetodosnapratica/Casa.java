7870f44f80a6a4828bd5115c308bbc2d65423594
Felipe L de Melo <felipe.leandro2@gmail.com>
2025-11-29 00:08:21 -0300

    Iniciando reciclagem de POO 'Programação Orientada a Objetos'

---
+++ ClasseseMetodosnaPratica/app/src/main/java/com/aprendendo/classesemetodosnapratica/Casa.java
@@ -0,0 +1,11 @@
+package com.aprendendo.classesemetodosnapratica;
+
+class Casa {
+    //propriedades
+    String cor;
+
+    //metodos
+    void abrirPorta(){
+         System.out.println("Porta aberta!");
+    }
+}
